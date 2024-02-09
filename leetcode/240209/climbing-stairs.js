b2bc9cc6148a136c0a8eaad30096098362b162ce
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-09 15:41:17 +1100

    climbing stairs

---
+++ leetcode/240209/climbing-stairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  const memo = new Map();
+
+  function dp(stepNumber) {
+    if (stepNumber <= 2) return stepNumber;
+
+    if (!memo.has(stepNumber)) {
+      memo.set(stepNumber, dp(stepNumber - 1) + dp(stepNumber - 2));
+    }
+
+    return memo.get(stepNumber);
+  }
+
+  function climb(n) {
+    return dp(n);
+  }
+
+  return climb(n);
+};
