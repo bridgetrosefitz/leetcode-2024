ec185928120bdc46f023abac52dcccc04b0aae6b
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-12 14:52:08 +1100

    two pointers; palindrome

---
+++ other/two-pointers/leetcode-two-pointers.js
@@ -0,0 +1,16 @@
+// Example 1: Given a string s, return true if it is a palindrome, false otherwise.
+
+// A string is a palindrome if it reads the same forward as backward.
+// That means, after reversing it, it is still the same string. For example: "abcdcba", or "racecar".
+
+const checkIfPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+};
