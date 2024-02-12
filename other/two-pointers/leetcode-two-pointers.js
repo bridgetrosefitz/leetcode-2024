5d5e394535119aa2bb7955bd81236453f1d927b8
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-12 14:55:59 +1100

    check for target in sorted array

--- other/two-pointers/leetcode-two-pointers.js
+++ other/two-pointers/leetcode-two-pointers.js
@@ -14,3 +14,26 @@ const checkIfPalindrome = function (s) {
   }
   return true;
 };
+
+// Example 2: Given a sorted array of unique integers
+// and a target integer, return true if there exists
+// a pair of numbers that sum to target, false otherwise.
+
+// For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13.
+
+const checkForTarget = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+    if (sum === target) return true;
+    if (sum > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return false;
+};
