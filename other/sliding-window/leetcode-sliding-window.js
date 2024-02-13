5e2455feb5bdd58275e8fa832d65528b2a204a08
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-13 13:30:30 +1100

    sliding window

---
+++ other/sliding-window/leetcode-sliding-window.js
@@ -0,0 +1,17 @@
+function findLength(nums, k) {
+  let left = 0,
+    curr = 0,
+    ans = 0;
+
+  for (right in nums) {
+    curr += nums[right];
+
+    while (current > k) {
+      current -= nums[left];
+    }
+
+    ans = max(ans, right - left + 1);
+  }
+
+  return ans;
+}
