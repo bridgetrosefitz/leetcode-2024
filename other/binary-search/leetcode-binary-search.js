9fb67d53d23711dd4a3b8dbf6d4f644d1d9be696
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-14 18:01:03 +1100

    binary search

---
+++ other/binary-search/leetcode-binary-search.js
@@ -0,0 +1,16 @@
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+};
