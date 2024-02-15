69108a4a1f0cffdadc2824f5f59f55ee344f2b6f
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-15 14:43:50 +1100

    find leftmost occurrence

--- other/binary-search/leetcode-binary-search.js
+++ other/binary-search/leetcode-binary-search.js
@@ -14,3 +14,19 @@ const binarySearch = (arr, target) => {
 
   return -1;
 };
+
+const binarySearchToReturnLeftmostTarget = (arr, target) => {
+  let left = 0;
+  let right = arr.length; // why does this need to start outside the bounds of the array
+  while (left < right) {
+    // why can't this be OR EQUAL
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] >= target) {
+      // the OR EQUAL above means we keep decrementing right
+      // until we find the first occurence of the target
+      right = mid;
+    } else {
+      left = mid + 1; // once we have moved one step further left than the target, return the element to the right (which will be the first occurrenc of the target)
+    }
+  }
+};
