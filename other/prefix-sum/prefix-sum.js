65c5efa47242ee593e2fa5ae45a21ff627fbfe24
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-16 16:08:48 +1100

    prefix sum

---
+++ other/prefix-sum/prefix-sum.js
@@ -0,0 +1,9 @@
+function createPrefixSum(arr) {
+  const prefix = [[nums[0]]];
+
+  for (let i = 1; i < nums.length; i++) {
+    prefix.push(nums[i] + prefix[prefix.length - 1]);
+  }
+
+  return prefix;
+}
