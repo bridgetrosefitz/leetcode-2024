dd97f74baf8422106967fb631d6db6914536513a
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-27 15:44:22 +1100

    bottom-up

--- leetcode/240227/house-robber.js
+++ leetcode/240227/house-robber.js
@@ -1,3 +1,5 @@
+// top-down
+
 const maximumProfitFromHouseRobbery = function (nums) {
   const memo = new Map();
 
@@ -12,4 +14,21 @@ const maximumProfitFromHouseRobbery = function (nums) {
   };
 
   return dp(nums.length - 1);
+};
+
+// bottom-up
+
+const maximumProfitFromHouseRobbery2 = function (nums) {
+  if (nums.length === 1) return nums[0];
+
+  const dp = new Array(nums.length).fill(0);
+
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+
+  for (let i = 2; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+  }
+
+  return dp[nums.length - 1];
 };
