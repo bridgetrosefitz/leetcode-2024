20007ede404041be090c32e694d33cebf1302ae4
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-27 14:44:43 +1100

    bottom up stairs

--- leetcode/240227/climbing-stairs.js
+++ leetcode/240227/climbing-stairs.js
@@ -1,3 +1,5 @@
+// top down
+
 const numberOfWaysToClimbStairs = function (n) {
   const dp = function (i) {
     const memo = new Map();
@@ -10,4 +12,20 @@ const numberOfWaysToClimbStairs = function (n) {
   };
 
   return dp(n);
+};
+
+// bottom up
+
+const numberOfWaysToClimnStairs2 = function (n) {
+  if (n === 1) return 1;
+
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
 };
