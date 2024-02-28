804082e981b038d665e334772d36d10f37b763b1
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-02-28 12:36:25 +1100

    bottom up

--- leetcode/240228/min-cost-climbing-stairs.js
+++ leetcode/240228/min-cost-climbing-stairs.js
@@ -2,6 +2,8 @@
  * @param {number[]} cost
  * @return {number}
  */
+
+// bottom-up
 var minCostClimbingStairs = function (cost) {
   const dp = new Array(cost.length + 1).fill(0);
 
@@ -10,4 +12,27 @@ var minCostClimbingStairs = function (cost) {
   }
 
   return dp[dp.length - 1];
+};
+
+// top-down
+
+var minCostClimbingStairs2 = function (cost) {
+  const memo = new Map();
+
+  const minimumCost = function (i) {
+    if (i <= 1) return 0;
+    if (!memo.has(i)) {
+      memo.set(
+        i,
+        Math.min(
+          minimumCost(i - 1) + cost[i - 1],
+          minimumCost(i - 2) + cost[i - 2]
+        )
+      );
+    }
+
+    return memo.get(i);
+  };
+
+  return minimumCost(cost.length);
 };
