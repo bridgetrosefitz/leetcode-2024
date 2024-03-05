c1fcc16fb63a43defe0a328354fb5bc6a802a644
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-03-06 08:49:02 +1100

    factorial recursive

--- colt-steele/factorial.js
+++ colt-steele/factorial.js
@@ -2,9 +2,14 @@
 
 function factorialIterative(num) {
   let total = 1;
-  for (let i = num; i > 0; i--) {
+  for (let i = num; i > 1; i--) {
     total *= i;
   }
 
   return total;
+}
+
+function factorialRecursive(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
 }
