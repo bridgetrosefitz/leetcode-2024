a34f2ce0a471d6e086082d9261c275cc91fbc3ed
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-03-13 07:51:01 +1100

    update nested sum with my own code

--- colt-steele/recursion/nested-even-sum.js
+++ colt-steele/recursion/nested-even-sum.js
@@ -2,10 +2,11 @@ function nestedEvenSum(obj) {
   let total = 0;
 
   function helper(helperObj) {
-    for (item of helperObj) {
-      if (item.constructor.name === "Object") {
+    for (const key in helperObj) {
+      const item = helperObj[key];
+      if (item instanceof Object) {
         helper(item);
-      } else if (item % 2 === 0) {
+      } else if (typeof item === "number" && item % 2 === 0) {
         total = total + item;
       }
     }
@@ -14,28 +15,3 @@ function nestedEvenSum(obj) {
   helper(obj);
   return total;
 }
-
-///////////ANSWER
-
-function nestedEvenSum2(obj, sum = 0) {
-  for (var key in obj) {
-    if (typeof obj[key] === "object") {
-      sum += nestedEvenSum2(obj[key]);
-    } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
-      sum += obj[key];
-    }
-  }
-  return sum;
-}
-
-var obj1 = {
-  outer: 2,
-  obj: {
-    inner: 2,
-    otherObj: {
-      superInner: 2,
-      notANumber: true,
-      alsoNotANumber: "yup",
-    },
-  },
-};
