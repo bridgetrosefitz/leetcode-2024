2cd900e59ff5e29d8883c9f40d5a2a780e18c687
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-03-15 16:06:36 +1100

    inorder

---
+++ leetcode/240315/binary-tree-inorder-traversal.js
@@ -0,0 +1,13 @@
+var inorderTraversal = function (root) {
+  const res = [];
+
+  function traverse(node) {
+    if (!node) return;
+    if (node.left) traverse(node.left);
+    res.push(node.val);
+    if (node.right) traverse(node.right);
+  }
+
+  traverse(root);
+  return res;
+};
