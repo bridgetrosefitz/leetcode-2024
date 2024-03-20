9c34480b0e54e346cff0fa6d70f7cedd679a44fd
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-03-20 13:10:11 +1100

    nyc coders; flip an image

---
+++ leetcode/240320/flipping-an-image.js
@@ -0,0 +1,10 @@
+var flipAndInvertImage = function (image) {
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+    for (let j = 0; j < image[i].length; j++) {
+      image[i][j] = 1 - image[i][j];
+    }
+  }
+
+  return image;
+};
