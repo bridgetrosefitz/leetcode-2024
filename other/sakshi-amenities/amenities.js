1e17c14a84a457f0c7e6e435f26a47e7fdc02979
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-04-08 11:46:32 +1000

    pseudocode for amenities

---
+++ other/sakshi-amenities/amenities.js
@@ -0,0 +1,16 @@
+function findOptimalBlock(blocks, amenities) {
+  // minWindowSize
+  // winningBlock
+  // left pointer
+  // right pointer
+  // amenities = map(amenities)
+  // while sum of amenities values < amenities.length
+  // for each amenity, if blocks[r] has it, increment count in map
+  // if each value in amenities is > 0, update winning block
+  // move right pointer
+  // while each value in amenities is > 0
+  // if window size is smaller than minWindowSize
+  // update minWindowSize
+  // increment left
+  // return winningBlock
+}
