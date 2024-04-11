eb8803ea010cf447276db4f5a55ebebb7a6cef4b
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-04-11 13:33:28 +1000

    kth smallest in bst; wrong answer

--- leetcode/240411/subsets.js
+++ leetcode/240411/subsets.js
@@ -2,23 +2,23 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums) {
-    const initialPath = []
-    const result = []
-    helper(0, initialPath, nums, result)
-    return result
+var subsets = function (nums) {
+  const initialPath = [];
+  const result = [];
+  helper(0, initialPath, nums, result);
+  return result;
 };
 
 function helper(i, path, nums, res) {
-    if(i === nums.length) {
-        res.push([...path])
-        return
-    }
+  if (i === nums.length) {
+    res.push([...path]);
+    return;
+  }
 
-    // exclude
-        helper(i + 1, path, nums, res)
-    // include
-        path.push(nums[i])
-        helper(i + 1, path, nums, res)
-        path.pop()
+  // exclude
+  helper(i + 1, path, nums, res);
+  // include
+  path.push(nums[i]);
+  helper(i + 1, path, nums, res);
+  path.pop();
 }
