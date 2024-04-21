e7b3cab64c291f4da6d0c8d8301e67461d8a85a3
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-04-21 17:46:11 +1000

    duplicate subtrees

---
+++ mock-interviews/240421-sakshi/duplicate-subtrees.js
@@ -0,0 +1,19 @@
+Function findDuplicateSubtrees(root) {
+	Const ans = []
+	Const subtrees = new Set([4], [4,2], [4,2,1])	
+	Const currentTree = [4, 2, 1]
+
+	dfs(node) {
+		
+		// when node is null
+			// add null to currentTree
+			// return
+		// visit left node
+		// add node value to currentTree
+		// check if tree is already in Set
+			// add node to ans
+		// add currentTree to Set
+		// visit right
+}
+}
+
