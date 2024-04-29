591cf652b3f27e64cc85c76cfa615cf279c40fa6
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-04-29 15:12:58 +1000

    infinite scroll first attempt - no text editor

---
+++ mock-interviews/240429-infinite-scroll/index.js
@@ -0,0 +1,48 @@
+const pokemonContainer = document.getElementById("pokemon-container");
+
+let latestPokemon = 0;
+
+function fetchPokemon(offset, limit) {
+  const fetchUrl = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}?offset=${offset}`;
+
+  return fetch(fetchUrl)
+    .then(res => res.json())
+    .then(data => {
+      latestPokemon += 20;
+      return data.results;
+    });
+}
+
+function appendPokemonSquares(pokemonData, container) {
+  const squares = pokemonData.map(
+    pokemon =>
+      `<div class=”pokemon-square”>
+${pokemon.name}
+</div>`
+  );
+
+  container.innerHtml += squares;
+}
+
+pokemonContainer.addEventListener("scroll", e => {
+  const { scrollHeight, scrollTop, clientHeight } = e.target;
+
+  if (scrollHeight - scrollTop - clientHeight === 0) {
+    fetchPokemon(latestPokemon, 20).then(pokemon =>
+      appendPokemonSquares(pokemon, pokemonContainer)
+    );
+  }
+});
+
+function addLoadingExperience() {
+  ///////////DO THIS
+}
+
+fetchPokemon(latestPokemon, 20).then(pokemon =>
+  appendPokemonSquares(pokemon, pokemonContainer)
+);
+
+////////////////EDGE CASES /////////////////////////
+
+// When we reach the last pokemon and < 20 come back + there are no more to fetch
+// Error handling
