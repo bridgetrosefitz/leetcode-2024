d91fc6acd9b35edc4357e722c241f65a11e354a8
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-05-09 10:26:56 +1000

    dfs recursive again

---
+++ leetcode/240509/2-island-count-dfs-recursive.js
@@ -0,0 +1,31 @@
+function getNumberOfIslands(binaryMatrix) {
+  let res = 0;
+
+  function dfs(x, y) {
+    if (isInvalidCell(x, y, binaryMatrix)) return;
+
+    if (binaryMatrix[x][y] === 0) return;
+
+    binaryMatrix[x][y] = 0;
+
+    dfs(x + 1, y);
+    dfs(x - 1, y);
+    dfs(x, y + 1);
+    dfs(x, y - 1);
+  }
+
+  for (let i = 0; i < binaryMatrix.length; i++) {
+    for (let j = 0; j < binaryMatrix[0].length; j++) {
+      if (binaryMatrix[i][j] === 1) {
+        res++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return res;
+}
+
+function isInvalidCell(x, y, matrix) {
+  return x < 0 || x > matrix.length - 1 || y < 0 || y > matrix[0].length;
+}
