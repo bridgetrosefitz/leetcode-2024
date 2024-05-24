c53b369920f2770aa27418a62261d08944908498
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-05-24 14:53:48 +1000

    backspace string compare using stackgit add .

---
+++ leetcode/240524/backspace-string-compare.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+var backspaceCompare = function (s, t) {
+  function getFinalString(s) {
+    const stack = [];
+
+    for (const ch of s) {
+      if (ch !== "#") {
+        stack.push(ch);
+      } else {
+        stack.pop();
+      }
+    }
+
+    return stack.join("");
+  }
+
+  const resS = getFinalString(s);
+  const resT = getFinalString(t);
+
+  return resS === resT;
+};
