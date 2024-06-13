6346ef386fcd58fa804a62370afd98d96140b586
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-06-13 15:56:00 +1000

    non TLE solution, maze

--- leetcode/240613/nearest-exit-from-entrance-in-maze.js
+++ leetcode/240613/nearest-exit-from-entrance-in-maze.js
@@ -8,46 +8,53 @@ const encode = (row, col) => `${row}-${col}`;
 
 var nearestExit = function (maze, entrance) {
   const dirs = [
-    [-1, 0],
     [1, 0],
-    [0, -1],
     [0, 1],
+    [-1, 0],
+    [0, -1],
   ];
-  const q = [];
-  q.push([...entrance, 0]);
+  const q = [entrance];
   const visited = new Set();
+  let steps = 0;
 
-  while (q.length) {
-    const [row, col, steps] = q.shift();
+  while (q.length > 0) {
+    const length = q.length;
+    for (let i = 0; i < length; i++) {
+      const [row, col] = q.shift();
+      const pos = `${row},${col}`;
+      if (visited.has(pos)) continue;
+      visited.add(pos);
 
-    visited.add(encode(row, col));
-
-    if (
-      row === 0 ||
-      col === 0 ||
-      row === maze.length - 1 ||
-      col === maze[0].length - 1
-    ) {
-      if (!(row === entrance[0] && col === entrance[1])) {
+      if (
+        (row === 0 ||
+          col === 0 ||
+          row === maze.length - 1 ||
+          col === maze[0].length - 1) &&
+        !(row === entrance[0] && col === entrance[1])
+      ) {
         return steps;
+      }
+
+      for (const [dR, dC] of dirs) {
+        const newRow = row + dR;
+        const newCol = col + dC;
+
+        if (
+          newRow < 0 ||
+          newCol < 0 ||
+          newRow >= maze.length ||
+          newCol >= maze[0].length
+        )
+          continue;
+        const newPos = `${newRow},${newCol}`;
+        if (visited.has(newPos)) continue;
+        if (maze[newRow][newCol] === "+") continue;
+
+        q.push([newRow, newCol]);
       }
     }
 
-    for (const [dR, dC] of dirs) {
-      const newRow = row + dR;
-      const newCol = col + dC;
-      if (
-        newRow < 0 ||
-        newCol < 0 ||
-        newRow >= maze.length ||
-        newCol >= maze[0].length
-      )
-        continue;
-      if (maze[newRow][newCol] === "+") continue;
-      if (visited.has(encode(newRow, newCol))) continue;
-
-      q.push([newRow, newCol, steps + 1]);
-    }
+    steps++;
   }
 
   return -1;
