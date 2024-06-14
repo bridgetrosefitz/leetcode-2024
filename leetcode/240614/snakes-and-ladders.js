67d12443ea6a13aaccb4d10a99531d21a2e25d8a
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-06-14 22:24:02 +0930

    snakes and ladders wip

---
+++ leetcode/240614/snakes-and-ladders.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} board
+ * @return {number}
+ */
+
+// edge cases
+// you do multiple extra up / down in a row
+var snakesAndLadders = function (board) {
+  // min
+  // array to memoise min rolls for each square
+  // q
+  // put [0,0] in q
+  // for i, between 0 - 5
+  // get cell value from adding i
+  // if we already have value in memo, use it
+  // if cell value is row 0, col length[0] - 1, update min steps
+  // return min
+};
