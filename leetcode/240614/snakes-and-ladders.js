e5fb2057ccdd806b6d585b43b28bfccfb6adb1f3
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-06-17 07:36:38 +0930

    upated pseudocode snakes and ladders

--- leetcode/240614/snakes-and-ladders.js
+++ leetcode/240614/snakes-and-ladders.js
@@ -3,16 +3,26 @@
  * @return {number}
  */
 
-// edge cases
-// you do multiple extra up / down in a row
+function getCellCoords(cellNumber, matrix) {
+  // subtract 1
+  const cols = matrix[0].length;
+  const rows = matrix.length;
+  const row = cellNumber % cols;
+  const col = Math.floor(cellNumber / cols);
+  return [row, col];
+}
+
 var snakesAndLadders = function (board) {
   // min
-  // array to memoise min rolls for each square
+  // array to memoise min rolls for each square, filled with Infinity
   // q
   // put [0,0] in q
   // for i, between 0 - 5
   // get cell value from adding i
-  // if we already have value in memo, use it
-  // if cell value is row 0, col length[0] - 1, update min steps
+  // if cell value is row 0, col length[0] - 1, update min steps with 1
+  // min steps = min of steps + 1 and current value in memo
+  // while value in cell is not -1
+  // get new cell value from cell
+  // min steps = min of steps + 1 and current value in memo
   // return min
 };
