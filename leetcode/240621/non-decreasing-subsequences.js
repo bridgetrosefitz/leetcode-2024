2fa0b25655107aeb9a1ded454ec448bac99840b9
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-06-21 12:28:32 +1000

    non decreasing subsequences passing

--- leetcode/240621/non-decreasing-subsequences.js
+++ leetcode/240621/non-decreasing-subsequences.js
@@ -1,30 +1,31 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-// [4, 6, 7, 7]
-
 var findSubsequences = function (nums) {
   const temp = [];
-  const res = new Set();
+  const res = new Set(); // make this a set, if the array has a bunch of duplicates, it will be hard to know to only do the same number once, so just use a set
 
   function backtrack(i) {
     // base case
-    if (temp.length >= 2) res.add(JSON.stringify(temp));
     if (i === nums.length) return;
 
     // work
-    if (!temp[i - 1] || nums[i] >= temp[i - 1]) {
+    if (temp.length === 0 || nums[i] >= temp[temp.length - 1]) {
       temp.push(nums[i]);
+    } else {
+      return;
     }
 
+    if (temp.length >= 2) res.add(JSON.stringify(temp));
+
     // backtrack
-    backtrack(i + 1);
+    for (let j = i + 1; j < nums.length; j++) {
+      backtrack(j);
+    }
+
     temp.pop();
-    backtrack(i + 1);
   }
 
-  backtrack(0);
+  for (let i = 0; i < nums.length; i++) {
+    backtrack(i);
+  }
 
   return [...res].map(JSON.parse);
 };
