06c5149cb7d013a2ab3292101c1209e1049dd084
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-06 08:29:52 +1000

    flexible shortest

---
+++ algo-monster/templates/Sliding-Window-Flexible-Shortest/240706-sliding-window-flexible-shortest.js
@@ -0,0 +1,22 @@
+// A "brute force" method is to use largest_sum_subarray_k as a subroutine
+// to check whether there exists a subarray of each possible length k with a sum larger than target,
+// and find the smallest k.
+// WHAT???
+
+function subarraySumShortest(nums, target) {
+  let windowSum = 0;
+  let length = nums.length;
+  let left = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    windowSum = windowSum + nums[right];
+
+    while (windowSum >= target) {
+      length = Math.min(length, right - left + 1);
+      windowSum = windowSum = nums[left];
+      left++;
+    }
+  }
+
+  return length;
+}
