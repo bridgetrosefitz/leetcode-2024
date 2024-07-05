4ca70bc57521f6da45a8f4009e38646f5776adea
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-05 10:40:40 +1000

    pseudocode wip for circular game

---
+++ leetcode/240705/find-the-winner-of-the-circular-game.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findTheWinner = function (n, k) {
+  // start = 1
+  // targetSteps
+  // steps = 1
+  // toDelete
+  // set
+  //  while set.size > 1
+  // if(k > set.size) target = k - set.size
+  // else, target = k
+  // for each el in set
+  // if(steps === targetSteps)
+  // toDelete = curr el
+  // if steps === targetSteps + 1
+  // start = curr el
+  // start = curr el +
+  // delete curr el from set
+  // [1, 2, 3, 4, 5], k = 2
+  //
+  // [1, 2, 3], k = 5
+};
