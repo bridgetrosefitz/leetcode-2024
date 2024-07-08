4e1cf4f9d06f805b900e553870c47c06811249cb
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-08 17:26:36 +1000

    comments

--- algo-monster/templates/Sliding-Window-Flexible-Longest/240708-sliding-window-flexible-longest.js
+++ algo-monster/templates/Sliding-Window-Flexible-Longest/240708-sliding-window-flexible-longest.js
@@ -6,10 +6,14 @@ function flexibleLongest(arr, target) {
   for (let right = 0; right < arr.length; right++) {
     total += arr[right];
 
+    // ensure we get a valid answer
     while (total > target) {
       total -= arr[left];
       left++;
     }
+
+    // once we've made it past the above check,
+    // we know we have a valid answer
 
     max = Math.max(max, right - left + 1);
   }
