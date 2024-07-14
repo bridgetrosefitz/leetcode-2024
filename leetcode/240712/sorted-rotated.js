30e49ea80bd132792b08be91b57b6337d555cdee
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-15 06:28:03 +1000

    sorted rotated

--- leetcode/240712/sorted-rotated.js
+++ leetcode/240712/sorted-rotated.js
@@ -1,24 +1,24 @@
-if (nums.length === 1) return nums[0];
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let currMin = Infinity;
 
-let left = 0,
-  right = nums.length - 1;
-
-// if array is already sorted without rotation, return the lowest number
-if (nums[right] > nums[0]) return nums[0];
-
-// binary search
-while (right >= left) {
-  const mid = Math.floor(left + (right - left) / 2);
-
-  // find the inflection point
-  if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
-  if (nums[mid - 1] > nums[mid]) return nums[mid];
-
-  if (nums[mid] > nums[0]) {
-    // the left portion is still in order, therefore the inflection is to the right
-    left = mid + 1;
-  } else {
-    // the inflection point is to the left
-    right = mid - 1;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[right] >= nums[left]) {
+      return currMin > nums[left] ? nums[left] : currMin;
+    }
+    if (nums[mid] >= nums[left]) {
+      left = mid + 1;
+    } else {
+      currMin = nums[mid];
+      right = mid - 1;
+    }
   }
-}
+
+  return -1;
+};
