f4262cd6a44cc600b5b57c3b1ad01bad58586909
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-15 13:32:13 +1000

    sqrt v2

--- leetcode/240715/square-root.js
+++ leetcode/240715/square-root.js
@@ -16,3 +16,22 @@ function squareRoot(n) {
   if (currentClosest * currentClosest !== n) return currentClosest - 1;
   return currentClosest;
 }
+
+function squareRoot2(n) {
+  let left = 0;
+  let right = n;
+  let currentClosest = -1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (mid ** 2 <= n) {
+      currentClosest = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return currentClosest;
+}
