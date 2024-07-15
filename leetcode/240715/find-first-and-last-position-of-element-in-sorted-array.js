7bc2515fda4714ddb265d5a00823f78abeeea6c0
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-15 20:17:25 +1000

    first and last position

---
+++ leetcode/240715/find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let firstIndex = -1;
+  let lastIndex = -1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] >= target) {
+      if (nums[mid] === target) firstIndex = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  // if there is only one element, return firstIndex as lastIndex also
+  if (firstIndex === nums.length - 1) return [firstIndex, firstIndex];
+
+  // if target is not present in the array (no need to go further)
+  if (firstIndex === -1) return [firstIndex, lastIndex];
+
+  // if there is no element greater than target, hmmmmmm
+
+  left = 0;
+  right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] > target) {
+      lastIndex = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return [firstIndex, lastIndex - 1];
+};
