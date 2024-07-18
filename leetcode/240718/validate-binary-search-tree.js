d082d18af8d34c595b46df68962f9ad6be9199b2
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-18 21:02:09 +1000

    validate bst

---
+++ leetcode/240718/validate-binary-search-tree.js
@@ -0,0 +1,15 @@
+function validateBST(root) {
+  function isValid(node, min, max) {
+    if (!node) return true;
+
+    if (node.val <= min || node.val >= max) {
+      return false;
+    }
+
+    return (
+      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+    );
+  }
+
+  return isValid(root, -Infinity, Infinity);
+}
