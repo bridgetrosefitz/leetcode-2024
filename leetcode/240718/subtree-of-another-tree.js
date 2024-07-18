b4df3a0aa92d8be0b226fc42f6bfd56d578fc5e3
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-18 20:48:30 +1000

    subtree

---
+++ leetcode/240718/subtree-of-another-tree.js
@@ -0,0 +1,21 @@
+function isSubtree(root, subRoot) {
+  function dfs(node) {
+    if (!node) return false;
+    if (isIdentical(node, subRoot)) return true;
+    return dfs(node.left) || dfs(node.right);
+  }
+
+  function isIdentical(node1, node2) {
+    if (node1 === null || node2 === null) {
+      return node1 === null && node2 === null;
+    }
+
+    return (
+      node1.val === node2.val &&
+      isIdentical(node1.left, node2.left) &&
+      isIdentical(node1.right, node2.right)
+    );
+  }
+
+  return dfs(root);
+}
