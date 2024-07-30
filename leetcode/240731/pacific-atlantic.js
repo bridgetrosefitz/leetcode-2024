b1421f6028a7c5763ff5e2623515cef28fcccbc9
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-31 07:48:13 +1000

    pac atl

---
+++ leetcode/240731/pacific-atlantic.js
@@ -0,0 +1,43 @@
+function pacificAtlantic(heights) {
+  const pac = "pac";
+  const atl = "atl";
+  const canReach = {
+    pac: new Set(),
+    atl: new Set(),
+  };
+
+  function dfs(row, col, prevHeight, ocean) {
+    if (row < 0 || col < 0 || row >= heights.length || col >= heights[0].length)
+      return;
+
+    if (canReach[ocean].has(`${row}-${col}`)) return;
+    if (heights[row][col] < prevHeight) return;
+    canReach[ocean].add(`${row}-${col}`);
+
+    dfs(row + 1, col, heights[row][col], ocean);
+    dfs(row - 1, col, heights[row][col], ocean);
+    dfs(row, col + 1, heights[row][col], ocean);
+    dfs(row, col - 1, heights[row][col], ocean);
+  }
+
+  for (let i = 0; i < heights.length; i++) {
+    dfs(i, 0, -1, pac);
+    dfs(i, heights[0].length, -1, atl);
+  }
+
+  for (let i = 0; i < heights[0].length; i++) {
+    dfs(0, i, -1, pac);
+    dfs(heights.length, i, -1, atl);
+  }
+
+  const intersection = [];
+
+  for (const pacReach of canReach[pac]) {
+    if (canReach[atl].has(pacReach)) {
+      const [row, col] = pacReach.split("-");
+      intersection.push([parseInt(row), parseInt(col)]);
+    }
+  }
+
+  return intersection;
+}
