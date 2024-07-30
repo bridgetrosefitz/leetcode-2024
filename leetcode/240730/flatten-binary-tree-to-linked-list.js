288b42e9591cbb4d257e4200b2b3d9a7a2461c5c
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-07-30 21:38:17 +1000

    flatten to linked list wip

---
+++ leetcode/240730/flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    if(!root) return null
+
+    function dfs(node) {
+        // base case
+        if(!node) return
+        if(!node.left) return node
+
+        const right = node.right
+        node.right = node.left 
+        const tail = dfs(node.left)
+        tail.right = right
+        // dfs(node.right)
+    }
+
+    dfs(root)
+    
+    return root
+};
