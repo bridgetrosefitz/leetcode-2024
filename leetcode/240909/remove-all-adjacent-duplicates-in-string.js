d6627b6b8fd2375410767e975d0f39301ad7f79d
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-09-09 20:54:40 +1000

    remove adjacent duplicates in string

---
+++ leetcode/240909/remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,13 @@
+function removeDuplicates(s) {
+  const stack = [];
+
+  for (const ch of s) {
+    if (stack.length && stack[stack.length - 1] === ch) {
+      stack.pop();
+      continue;
+    }
+    stack.push(ch);
+  }
+
+  return stack.join("");
+}
