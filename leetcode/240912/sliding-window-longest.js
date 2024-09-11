1f746523d67c73a0fbdacf1e548aafa603b1db03
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-09-12 09:09:41 +1000

    sliding window longest

---
+++ leetcode/240912/sliding-window-longest.js
@@ -0,0 +1,22 @@
+function slidingWindowLongest(arr, target) {
+  let total = 0;
+  let max = 0;
+  let left = 0;
+
+  for (let right = 0; right < arr.length; right++) {
+    total += arr[right];
+
+    // if we have made our ans invalid, go in the loop to make it valid again
+
+    while (total > target) {
+      total -= arr[left];
+      left++;
+    }
+
+    // once we've made it here, we know we have a valid answer
+
+    max = Math.max(max, right - left + 1);
+  }
+
+  return max;
+}
