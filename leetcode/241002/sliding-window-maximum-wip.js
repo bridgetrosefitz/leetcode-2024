ee93846b6991913adb8383a3c162c060614d8b50
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-10-02 08:49:28 +1000

    sliding window max

---
+++ leetcode/241002/sliding-window-maximum-wip.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+
+// FROM PRIOR
+var maxSlidingWindow = function (nums, k) {
+  // make window
+  // output []
+  // put first max in output
+  // subarray
+  // create a heap of size k > each time you move up you'll spend logk to push in the new thing
+  // loop from right = windowSize to right < nums.length
+  // add right el to subarray
+  // remove left el from subarray
+  // get max of new subarray
+  // push max into output
+  // return output
+};
