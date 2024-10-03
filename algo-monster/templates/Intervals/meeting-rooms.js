925757b07c4709222d549e1e74c35580c7a827ad
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-10-04 08:58:36 +1000

    intervals template

---
+++ algo-monster/templates/Intervals/meeting-rooms.js
@@ -0,0 +1,9 @@
+function canAttendMeetings(intervals) {
+  // if 1 or 0 intervals, return true
+  // sort ascending by start time
+  // set up first interval for comparison
+  // starting at second, iterate over every interval
+  // >> do the 'interesting part', e.g. checking for overlap (return false if overlap)
+  // >> pull out current start and end values
+  // return true
+}
