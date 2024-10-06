0286f52273836341e47cb9a9b10996d17ae9ab61
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-10-07 07:29:47 +1100

    interval list intersections wip

---
+++ leetcode/241007/interval-list-intersections.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} firstList
+ * @param {number[][]} secondList
+ * @return {number[][]}
+ */
+var intervalIntersection = function(firstList, secondList) {
+
+    const res = []
+
+    // ---
+    // - -- 
+
+    // ---
+    //    -
+
+    // --
+    //  --
+
+    // set currSecond to first element of secondList
+    // iterate left to right over firstList (currFirst)
+        // while currSecond start >= currFirst start OR currSecond end <= currFirst end
+            // get intersection start SOMEHOW
+            // get intersection end SOMEHOW
+    
+    return res    
+};
