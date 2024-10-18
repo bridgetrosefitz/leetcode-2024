9279d78abf53f897cd885aa79e4f2952b2a418f9
bridgetrosefitz <bridgetrosefitz@gmail.com>
2024-10-18 16:21:48 +1100

    more wip diagonal traverse

--- leetcode/241018/diagonal-traverse.js
+++ leetcode/241018/diagonal-traverse.js
@@ -1,30 +1,81 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
 var findDiagonalOrder = function (mat) {
-  const upRightVector = [-1, 1];
-  const downLeftVector = [1, -1];
-  const rightVector = [0, 1];
-  const downVector = [1, 0];
-  let currDir = "upRight";
   const res = [];
-  let count = mat.length * mat[0].length;
+  let counter = mat.length * mat[0].length;
+  console.log("counter", counter);
+  let currDir = "upRight";
+  const vectors = {
+    upRight: [-1, 1],
+    downLeft: [1, -1],
+  };
+  let currRow = 0;
+  let currCol = 0;
 
-  function helper(row, col, dir) {
-    // if count is 0, return // TO DO CHECK IF WE RETURN AT 0 or -1
-    // if out of bounds, return
-    // push val into res
-    // decrement count
-    // queue up next calls
-    // make newRow and newCol acc to dir
-    // if dir is upRight
-    // make newRow and newCol with right vector
-    // make newRow and newCol with dow
+  while (counter > 0) {
+    console.log("hi");
+    res.push(mat[currRow][currCol]);
+    currRow = currRow + vectors[currDir][0];
+    currCol = currCol + vectors[currDir][1];
+
+    // Top right corner
+    if (currRow < 0 && currCol === mat.length) {
+      currRow += 2;
+      currCol -= 1;
+      currDir = "downLeft";
+      counter--;
+      continue;
+    }
+    // Bottom left corner
+    if (currRow === mat.length && currCol < 0) {
+      currRow -= 1;
+      currCol += 2;
+      currDir = "upRight";
+      counter--;
+      continue;
+    }
+    // top of grid
+    if (currRow < 0) {
+      currRow += 1;
+      currDir = "downLeft";
+    }
+    // left of grid
+    if (currCol < 0) {
+      currCol += 1;
+      currDir = "upRight";
+    }
+    // bottom of grid
+    if (currRow === mat.length) {
+      currRow -= 1;
+      currCol += 2;
+      currDir = "upRight";
+    }
+    // right of grid
+    if (currCol > mat[0].length) {
+      currRow += 2;
+      currCol -= 1;
+      currDir = "downLeft";
+    }
+
+    counter--;
   }
-
-  helper(0, 0);
-  return res;
-
-  //=======================
-
+  // while there are still squares to visit (decrement from rows len x cols len)
+  // set currDirection to upRight
+  // go in currDirection until you go out of bounds
+  // if currDir is upRight, add [-1, 1] to cell
+  // if currDir is downLeft, add [1, -1] to cell
+  // decrement counter
+  // turn corner --- FIGURE OUT
   // while out of bounds
-  // try to go right
-  // try to go left
+  // WHEN GOING UP RIGHT
+  // try to go down one
+  // try to down two and left one
+  // WHEN GOING DOWN LEFT
+  // try to go right one
+  // try to go right two and up one
+  // toggle currDirection
+
+  return res;
 };
