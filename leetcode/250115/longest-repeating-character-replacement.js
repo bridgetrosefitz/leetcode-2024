6d332cb20d4d074103991134176d9a28c83bf9d8
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-01-15 13:58:05 +1100

    longest repeating ch replace

---
+++ leetcode/250115/longest-repeating-character-replacement.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {
+  /*
+
+    {
+        A: 2
+        B: 
+        C:
+    }
+
+    max = 4
+    diff = 1
+    "AABABBA"
+     L
+     R
+
+    diff = 0
+    "AABCDDA"
+     L
+       R
+    
+    */
+};
