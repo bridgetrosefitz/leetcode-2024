7f816b3f11ce46db27d1142dd955b90a560266e4
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-04-11 08:59:53 +0800

    react state implementation

---
+++ other/react-state/useState.js
@@ -0,0 +1,32 @@
+const React = () => {
+  let state = [];
+  let stateNextIndex = 0;
+
+  const useState = initialValue => {
+    // setter
+    // see if there is an existing value in stateValues at localIndex (but how do we remember localIndex from before)
+    // if nothing there, set it to initialValue
+    // return value and setter
+    const localIndex = stateNextIndex;
+
+    if (state[localIndex] === undefined) {
+      state[localIndex] = initialValue;
+    }
+
+    const setter = val => {
+      state[localIndex] = val;
+      render();
+    };
+
+    return [state[localIndex], setter];
+  };
+
+  const render = () => {
+    stateNextIndex = 0;
+    Component();
+  };
+
+  return {
+    useState,
+  };
+};
