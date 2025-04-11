63842e4496332b7f5db3b169241d51dff82b6c7f
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-04-11 08:56:52 +0800

    google mock v1

---
+++ mock-interviews/250410-google-mock/greater-than.js
@@ -0,0 +1,93 @@
+function isValid(nums) {
+  // build adj list
+
+  /*
+  
+    [
+    ["a", "b"],
+    ["a", "d"],
+    ["b", "d"],
+  ],
+
+  */
+  const adj = {};
+
+  for (const [node, neighbor] of nums) {
+    if (!adj[node]) {
+      adj[node] = [neighbor];
+    } else {
+      adj[node].push(neighbor);
+    }
+
+    if (!adj[neighbor]) {
+      adj[neighbor] = [];
+    }
+  }
+
+  /*
+    {
+  a: [b,d],
+  b: [d],
+  d: []
+  }
+  
+  */
+
+  const visited = new Set(); // {}
+
+  function dfs(node) {
+    if (visited.has(node)) {
+      return false;
+    }
+
+    visited.add(node);
+
+    for (const neighbor of adj[node]) {
+      if (!dfs(neighbor)) {
+        return false;
+      }
+    }
+
+    // when we come back from checking dfs on all a node's neighbors, remove it from visited (because you or your neighbors can no longer possibly point back to another visited node)
+    visited.delete(node);
+
+    return true;
+  }
+
+  for (const node of Object.keys(adj)) {
+    // clear visited
+    visited.clear();
+    if (!dfs(node)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const tests = [
+  [
+    ["a", "b"],
+    ["a", "d"],
+    ["b", "d"],
+  ],
+  [
+    ["a", "b"],
+    ["b", "c"],
+  ],
+  [
+    ["a", "b"],
+    ["b", "c"],
+    ["c", "a"],
+  ],
+];
+const expected = [true, true, false];
+
+for (let i = 0; i < tests.length; i++) {
+  const ans = isValid(tests[i]);
+  if (ans !== expected[i]) {
+    console.log("FAIL", "test", tests[i], "expected", expected[i]);
+  } else {
+    console.log("PASS");
+  }
+}
