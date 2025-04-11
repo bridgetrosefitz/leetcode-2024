d66586244711c5a3ab9a20bfdac46b3c709d0cf6
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-04-11 08:58:01 +0800

    binary search templates

---
+++ algo-monster/templates/Binary-Search/250410/binary-search-vanilla.js
@@ -0,0 +1,19 @@
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor(right - left) / 2 + left;
+    if (arr[mid] === target) {
+      return mid;
+    }
+
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
