c4e534340e6d09431702ba5082ca17604bb474e1
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-06-02 16:46:02 +0800

    meeting rooms

---
+++ leetcode/250602/meeting-rooms.js
@@ -0,0 +1,11 @@
+var canAttendMeetings = function (intervals) {
+  const sortedIntervals = [...intervals].sort((a, b) => a[0] - b[0]);
+
+  for (let i = 0; i < sortedIntervals.length - 1; i++) {
+    if (sortedIntervals[i][1] > sortedIntervals[i + 1][0]) {
+      return false;
+    }
+  }
+
+  return true;
+};
