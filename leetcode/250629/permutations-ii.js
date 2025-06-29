025bf3687001ed277f9421402ee31bdbbaa14ad9
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-06-29 07:26:55 -0400

    permutations ii

---
+++ leetcode/250629/permutations-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function (nums) {
+  const res = [];
+  const temp = [];
+  const options = new Map();
+
+  // build options
+  for (const num of nums) {
+    options.set(num, (options.get(num) || 0) + 1);
+  }
+
+  function buildPermutation() {
+    if (temp.length === nums.length) {
+      res.push([...temp]);
+      return;
+    }
+
+    for (const [num, frequency] of options.entries()) {
+      if (frequency > 0) {
+        temp.push(num);
+        options.set(num, frequency - 1);
+
+        buildPermutation();
+
+        temp.pop();
+        options.set(num, frequency);
+      }
+    }
+  }
+
+  buildPermutation();
+
+  return res;
+};
