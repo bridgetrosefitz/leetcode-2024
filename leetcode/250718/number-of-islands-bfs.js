4e3819eb8de1022f7c736d454d3cbc111fc78a73
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-07-18 09:46:37 -0400

    number of islands bfs

---
+++ leetcode/250718/number-of-islands-bfs.js
@@ -0,0 +1,54 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  const dirs = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+  let count = 0;
+  const q = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "1") {
+        count++;
+        q.push([i, j]);
+
+        while (q.length) {
+          const [row, col] = q.shift();
+          if (grid[row][col] === "0") {
+            continue;
+          } // THIS IS NEEDED TO STOP EARLY IF WE'VE PUSHED THE SAME CELL INTO THE QUEUE, BUT THAT CELL HAS BEEN CHANGED TO 0 SINCE BEING ADDED TO THE QUEUE
+
+          grid[row][col] = "0";
+
+          for (const [dR, dC] of dirs) {
+            const newRow = row + dR;
+            const newCol = col + dC;
+
+            if (
+              newRow < 0 ||
+              newCol < 0 ||
+              newRow >= grid.length ||
+              newCol >= grid[0].length
+            ) {
+              continue;
+            }
+
+            if (grid[newRow][newCol] === "0") {
+              continue;
+            }
+
+            q.push([newRow, newCol]);
+          }
+        }
+      }
+    }
+  }
+
+  return count;
+};
