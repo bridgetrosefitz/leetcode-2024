e9a216154aa229a9efdcf97d5064e73a8bb6361c
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-07-22 19:01:27 -0400

    improvements to add two numbers

--- leetcode/250722/add-two-numbers.js
+++ leetcode/250722/add-two-numbers.js
@@ -10,29 +10,47 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
+// const addTwoNumbers = function (l1, l2) {
+//   const dummy = new ListNode();
+//   let curr = dummy;
+//   let carry = 0; // 0
+
+//   while (l1 || l2) {
+//     const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry; // 2
+//     const newNode = new ListNode(sum % 10);
+//     carry = Math.floor(sum / 10);
+//     curr.next = newNode;
+//     curr = newNode;
+
+//     if (l1) {
+//       l1 = l1.next;
+//     }
+
+//     if (l2) {
+//       l2 = l2.next;
+//     }
+//   }
+
+//   if (carry > 0) {
+//     curr.next = new ListNode(1);
+//   }
+
+//   return dummy.next;
+// };
+
+const addTwoNumbers = function (l1, l2) {
   const dummy = new ListNode();
   let curr = dummy;
-  let carry = 0; // 0
+  let carry = 0;
 
-  while (l1 || l2) {
-    const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry; // 2
-    const newNode = new ListNode(sum % 10);
+  while (l1 || l2 || carry) {
+    const sum = (l1?.val || 0) + (l2?.val || 0) + carry;
     carry = Math.floor(sum / 10);
-    curr.next = newNode;
-    curr = newNode;
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
 
-    if (l1) {
-      l1 = l1.next;
-    }
-
-    if (l2) {
-      l2 = l2.next;
-    }
-  }
-
-  if (carry > 0) {
-    curr.next = new ListNode(1);
+    l1 = l1?.next;
+    l2 = l2?.next;
   }
 
   return dummy.next;
