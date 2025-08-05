88900e46366a25a19c48daaa375ec5f8627af0c7
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-08-05 18:02:54 -0400

    binary tree right side view

---
+++ leetcode/250805/binary-tree-right-side-view-bfs.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  const q = [root];
+  const res = [];
+
+  while (q.length) {
+    const countNodes = q.length;
+
+    for (let i = 0; i < countNodes; i++) {
+      const curr = q.shift();
+
+      if (i === countNodes - 1) {
+        res.push(curr.val);
+      }
+
+      if (curr.left) {
+        q.push(curr.left);
+      }
+
+      if (curr.right) {
+        q.push(curr.right);
+      }
+    }
+  }
+
+  return res;
+};
